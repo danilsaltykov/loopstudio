fce56484303cf8c86582493d09e05a09106fcc86
Danil <dddsaltykov@gmail.com>
2021-01-22 19:46:18 +0500

    some changes

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,22 @@
 let burger = document.querySelector('.header__burger');
 let headerContent = document.querySelector('.header__content');
+let body = document.body;
+
 
 
 burger.addEventListener('click', function(){
     burger.classList.toggle('active');
     headerContent.classList.toggle('active');
+    // body.style.overflowY = 'hidden'
+
 });
 
-console.log(headerContent)
+
+// if(body.style.overflowY === 'hidden' ) {
+//     body.style.overflowY = 'visible';
+// } else{
+//     console.log('hello')
+// }
+
+
+
